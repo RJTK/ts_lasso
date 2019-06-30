8bee351fc4e818cdf839b4e56f81993b6e44783a
Ryan J. Kinnear <Ryan@Kinnear.ca>
2019-06-30 17:26:10 -0400

    soft-threshold and cost-function.

--- test/test_ts_lasso.py
+++ test/test_ts_lasso.py
@@ -2,6 +2,36 @@ import unittest
 import numpy as np
 
 
+from ts_lasso.ts_lasso import soft_threshold
+
+
 class TestMain(unittest.TestCase):
     def test000(self):
         return
+
+    def test_soft_threshold001(self):
+        X = np.array([0, 1, 2, -1, -2, 0.5])
+        X_xp = np.array([0, 0, 1, 0, -1, 0])
+        X_st = soft_threshold(X, 1.0)
+        np.testing.assert_array_almost_equal(X_xp, X_st)
+        return
+
+    def test_soft_threshold002(self):
+        X = np.array([[0, 1, 2, -1, -2, 0.5],
+                      [0, 2, -1, -2, 1, -0.5]])
+        X_xp = np.array([[0, 0, 1, 0, -1, 0],
+                         [0, 1, 0, -1, 0, 0]])
+        X_st = soft_threshold(X, 1.0)
+        np.testing.assert_array_almost_equal(X_xp, X_st)
+        return
+
+    def test_soft_threshold003(self):
+        X = np.array([[[0, 1, 2, -1, -2, 0.5],
+                       [0, 2, -1, -2, 1, -0.5]]])
+        W = np.array([[[1.0, 0.5, 1.0, 1.0, 0.0, 1.0],
+                       [1.0, 2.0, 1.0, 1.0, 0.5, 0.25]]])
+        X_xp = np.array([[[0, 0.5, 1, 0, -2, 0],
+                          [0, 0, 0, -1.0, 0.5, -0.25]]])
+        return
+
+
--- ts_lasso/ts_lasso.py
+++ ts_lasso/ts_lasso.py
@@ -1,1 +1,51 @@
+import numba
 import numpy as np
+
+
+@numba.jit(nopython=True, cache=True)
+def cost_function(X, B, lmbda=0.0, W=None):
+    p, n, _ = B.shape
+    T, _ = X.shape
+    Xhat = np.zeros_like(X[p + 1:])
+    for t in range(p + 1, T):
+        Xhat[t] = predict(B, X[t - 1: t - p - 1: -1])
+    E = X - Xhat
+    cost = (1. / (2 * T)) * np.linalg.norm(E, "fro")
+
+    if lmbda != 0.0:
+        if W is not None:
+            cost = cost + lmbda * np.linalg.norm(B * W, 1)
+        else:
+            cost = cost + lmbda * np.linalg.norm(B, 1)
+    return cost
+
+
+@numba.jit(nopython=True, cache=True)
+def predict(B, X):
+    p, n = X.shape
+    Xhat = np.zeros(n)
+    for tau in range(p):
+        Xhat = Xhat + B[tau] @ X[p - tau]
+    return Xhat
+
+
+@numba.jit(nopython=True, cache=True)
+def soft_threshold(Z, tau, W=None):
+    """
+    Weighted soft-threshold.  This implements
+
+    prox_tau (W \cdot Z)
+    """
+    if W is None:
+        X = np.abs(Z) - tau
+    else:
+        X = np.abs(Z) - tau * W
+    return np.sign(Z) * X * (X >= 0)
+
+
+@numba.jit(nopython=True, cache=True)
+def descent_direction(B, R):
+    """
+    Returns a descent direction (i.e. grad g)
+    """
+    return np.zeros_like(B)
