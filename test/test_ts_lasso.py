1c71810b3d1d723b8f6f590df59fdfab9a2345c5
Ryan J. Kinnear <Ryan@Kinnear.ca>
2019-06-30 21:17:20 -0400

    Basic functions and some tests.

--- test/test_ts_lasso.py
+++ test/test_ts_lasso.py
@@ -2,10 +2,15 @@ import unittest
 import numpy as np
 
 
-from ts_lasso.ts_lasso import soft_threshold
+from ts_lasso.ts_lasso import (soft_threshold, predict,
+                               cost_function, cost_gradient)
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+        return
+
     def test000(self):
         return
 
@@ -34,4 +39,103 @@ class TestMain(unittest.TestCase):
                           [0, 0, 0, -1.0, 0.5, -0.25]]])
         return
 
+    def test_predict001(self):
+        X = np.array([[1.0, 2.0]])
+        B = np.array([[[2.2, 3.1],
+                       [-1.8, 2.6]]])
+        x_hat_xp = np.array([8.4, 3.4])
+        x_hat = predict(B, X)
+        np.testing.assert_array_almost_equal(x_hat_xp, x_hat)
+        return
 
+    def test_predict002(self):
+        X = np.array([[1.0, 2.0],
+                      [-1.0, -2.0]])
+        B = np.array([[[2.2, 3.1],
+                       [-1.8, 2.6]],
+                      [[4.4, 6.2],
+                       [-3.6, 5.2]]])
+        x_hat_xp = np.array([-8.4, -3.4])
+        x_hat = predict(B, X)
+        np.testing.assert_array_almost_equal(x_hat_xp, x_hat)
+        return
+
+    def test_predict003(self):
+        for _ in range(10):
+            X = np.random.normal(size=(5, 10))
+            B = np.random.normal(size=(5, 10, 10))
+            x_hat_xp = (B[0] @ X[0, :] +
+                        B[1] @ X[1, :] +
+                        B[2] @ X[2, :] +
+                        B[3] @ X[3, :] +
+                        B[4] @ X[4, :])
+            x_hat = predict(B, X)
+            np.testing.assert_array_almost_equal(x_hat_xp, x_hat)
+        return
+
+    def test_predict004(self):
+        zero = np.zeros(3)
+        B = np.random.normal(size=(2, 3, 3))
+        X = np.random.normal(size=(2, 3))
+        x_hat = predict(B, X[-1: max(0, -2)])
+        np.testing.assert_array_equal(zero, x_hat)
+        return
+
+
+    def test_cost_function001(self):
+        X = np.array([[1.0, 2.0],
+                      [-1.0, -2.0]])
+        B = np.array([[[2.2, 3.1],
+                       [-1.8, 2.6]]])
+        cost_xp = (1. / 4) * (np.sum((X[1] - B[0] @ X[0])**2) +
+                              np.sum(X[0]**2))
+        cost = cost_function(B, X, lmbda=0.0, W=None)
+        self.assertAlmostEqual(cost, cost_xp)
+        return
+
+
+    def test_cost_function002(self):
+        for _ in range(10):
+            X = np.random.normal(size=(3, 2))
+            B = np.array([[[2.2, 3.1],
+                           [-1.8, 2.6]]])
+            cost_xp = (1. / 6) * (np.sum((X[2] - B[0] @ X[1])**2) +
+                                  np.sum((X[1] - B[0] @ X[0])**2) +
+                                  np.sum(X[0]**2))
+            cost = cost_function(B, X, lmbda=0.0, W=None)
+            self.assertAlmostEqual(cost, cost_xp)
+        return
+
+    def test_cost_function003(self):
+        for _ in range(10):
+            X = np.random.normal(size=(3, 2))
+            B = np.array([[[2.2, 3.1],
+                           [-1.8, 2.6]]])
+            cost_xp = ((1. / 6) * (np.sum((X[2] - B[0] @ X[1])**2) +
+                                   np.sum((X[1] - B[0] @ X[0])**2) +
+                                   np.sum(X[0]**2)) +
+                       2 * np.sum(np.abs(B)))
+            cost = cost_function(B, X, lmbda=2.0, W=None)
+            self.assertAlmostEqual(cost, cost_xp)
+        return
+
+    def test_cost_function004(self):
+        for _ in range(10):
+            X = np.random.normal(size=(3, 2))
+            W = np.random.normal(size=(2, 2))
+            B = np.random.normal(size=(2, 2, 2))
+            cost_xp = ((1. / 6) * (
+                np.sum((X[2] - B[0] @ X[1] - B[1] @ X[0])**2) +
+                np.sum((X[1] - B[0] @ X[0])**2) +
+                np.sum(X[0]**2)) +
+                       2 * np.sum(np.abs(W * B)))
+            cost = cost_function(B, X, lmbda=2.0, W=W)
+            self.assertAlmostEqual(cost, cost_xp)
+        return
+
+    def test_cost_gradient000(self):
+        # This only checks that it compiles / runs.
+        B = np.random.normal(size=(3, 2, 2))
+        R = np.random.normal(size=(3, 2, 2))
+        cost_gradient(B, R)
+        return
--- ts_lasso/ts_lasso.py
+++ ts_lasso/ts_lasso.py
@@ -2,31 +2,56 @@ import numba
 import numpy as np
 
 
+def solve_lasso(X, p, lmbda=0.0, W=None, step_rule=0.1,
+                line_srch=None):
+    return
+
+
 @numba.jit(nopython=True, cache=True)
-def cost_function(X, B, lmbda=0.0, W=None):
+def _basic_prox_descent(X, B0, lmbda, ss=0.1, W=None):
+    """
+    Most basic constant stepsize proximal gradient scheme.
+    """
+    return
+
+
+@numba.jit(nopython=True, cache=True)
+def cost_function(B, X, lmbda=0.0, W=None):
     p, n, _ = B.shape
     T, _ = X.shape
-    Xhat = np.zeros_like(X[p + 1:])
-    for t in range(p + 1, T):
-        Xhat[t] = predict(B, X[t - 1: t - p - 1: -1])
-    E = X - Xhat
-    cost = (1. / (2 * T)) * np.linalg.norm(E, "fro")
+    err = np.sum(X[0]**2)
+    for t in range(1, T):
+        if t - p - 1 < 0:
+            x_hat = predict(B, X[t - 1::-1])
+        else:
+            x_hat = predict(B, X[t - 1: t - p - 1: -1])
+        err = err + np.sum((X[t] - x_hat)**2)
+    cost = (1. / (2 * T)) * err
 
     if lmbda != 0.0:
         if W is not None:
-            cost = cost + lmbda * np.linalg.norm(B * W, 1)
+            cost = cost + lmbda * l1_norm(B * W)
         else:
-            cost = cost + lmbda * np.linalg.norm(B, 1)
+            cost = cost + lmbda * l1_norm(B)
     return cost
 
 
 @numba.jit(nopython=True, cache=True)
 def predict(B, X):
+    """
+    Calculates sum_tau B[tau] @ X[t - tau, :].  The array X should
+    be in the order [X[t - 1], X[t - 2], ..., X[t - p].
+    """
     p, n = X.shape
     Xhat = np.zeros(n)
     for tau in range(p):
-        Xhat = Xhat + B[tau] @ X[p - tau]
+        Xhat = Xhat + B[tau] @ X[tau, :]
     return Xhat
+
+
+@numba.jit(nopython=True, cache=True)
+def l1_norm(B):
+    return np.sum(np.abs(B))
 
 
 @numba.jit(nopython=True, cache=True)
@@ -44,8 +69,18 @@ def soft_threshold(Z, tau, W=None):
 
 
 @numba.jit(nopython=True, cache=True)
-def descent_direction(B, R):
+def cost_gradient(B, R):
     """
     Returns a descent direction (i.e. grad g)
     """
-    return np.zeros_like(B)
+    p, n, _ = B.shape
+    grad = np.zeros_like(B)
+
+    for s in range(p):
+        grad[s] = R[s]
+        for tau in range(p):
+            if tau - s < 0:
+                grad[s] = grad[s] + R[tau - s].T @ B[tau]
+            else:
+                grad[s] = grad[s] + R[tau - s] @ B[tau]
+    return grad
